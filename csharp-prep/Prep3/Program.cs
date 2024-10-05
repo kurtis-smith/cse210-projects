36442d1e169ac2eefbd30a0938362ec0387be6cc
kurtis-smith <smithkurtis2004@gmail.com>
2024-10-05 00:23:23 -0600

    prep 3 commit

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Dynamic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        bool blr = true;
+
+        while(blr == true) {
+        
+            Random randomGenerator = new Random();
+            int mn = randomGenerator.Next(1, 101);
+
+            bool loopRunner = true;
+
+            int gc = 1;
+
+            while(loopRunner == true) {
+
+                Console.Write("What is your guess: ");
+                int g = int.Parse(Console.ReadLine());
+
+                if(g == mn) {
+                    loopRunner = false;
+                } else if(g > mn) {
+                    Console.WriteLine("Lower");
+                    gc++;
+                } else if(g < mn) {
+                    Console.WriteLine("Higher");
+                    gc++;
+                } 
+
+            }
+
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"Guess Count: {gc}\n");
+            Console.Write("Do you want to play again: ");
+            string response = Console.ReadLine();
+
+            Console.WriteLine("");
+
+            if(response != "yes") {
+                blr = false;
+            }
+
+        }
+
     }
 }
